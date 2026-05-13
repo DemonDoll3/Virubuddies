75641af57e5d35a29442c6cd24db827819db2a6c
Demon <101758493+DemonDoll3@users.noreply.github.com>
2026-05-13 13:50:40 -0400

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -15,9 +15,9 @@ let charadex = {};
 /* Any preview links will still show Charadex's information
 /* ==================================================================== */
 charadex.site = {
-  title: "Charadex",
-  url: "https://charadex-team.github.io/charadex-v1.0/",
-  description: `A tool for organizing small ARPGs and species.`
+  title: "Virubuddies",
+  url: "https://demondoll3.github.io/Virubuddies/",
+  description: `Masterlist for the Virubuddies species.`
 }
 
 /* ==================================================================== */
@@ -26,7 +26,7 @@ charadex.site = {
 /* ==================================================================== */
 charadex.sheet = {
 
-  id: "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak",
+  id: "1WoNDj8qWmNPsC1BS-IxQWt8qxdYkNFwiTPRkLAtwZT8",
 
   pages: {
     masterlist:    "masterlist",
